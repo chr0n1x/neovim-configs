235baa9fe65bb8a5e2ad44d7da71a55f16cf0ac1
Kevin Ran <kran@adobe.com>
2024-07-30 16:16:41 -0400

    fix: adjust lsp actions, fix live_grep

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,18 +4,18 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
   callback = function(event)
-    local opts = {buffer = event.buf}
+    vim.keymap.set('n', '<leader>l', function() vim.lsp.buf.hover() end,              { buffer = event.buf, desc = 'LSP commands (default: show symbol def)' })
+    vim.keymap.set('n', '<leader>ld', function() vim.lsp.buf.definition() end,        { buffer = event.buf, desc = 'LSP go to definition' })
+    vim.keymap.set('n', '<leader>lh', function() vim.lsp.buf.signature_help() end,    { buffer = event.buf, desc = 'LSP signature help' })
+    vim.keymap.set('n', '<leader>lw', function() vim.lsp.buf.workspace_symbol() end,  { buffer = event.buf, desc = 'LSP search for symbol in workspace' })
 
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', '<leader>ld', function() vim.diagnostic.open_float() end,     { buffer = event.buf, desc = 'LSP open diagnostics' })
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end,              { buffer = event.buf, desc = 'LSP go to next diagnostic' })
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end,              { buffer = event.buf, desc = 'LSP go to prev diagnostic' })
+
+    vim.keymap.set('n', '<leader>lb', function() vim.lsp.buf.references() end,        { buffer = event.buf, desc = 'LSP buffer/edit symbol actions (default: show refs for current symbol)' })
+    vim.keymap.set('n', '<leader>lba', function() vim.lsp.buf.code_action() end,      { buffer = event.buf, desc = 'LSP code action' })
+    vim.keymap.set('n', '<leader>lbr', function() vim.lsp.buf.rename() end,           { buffer = event.buf, desc = 'LSP rename' })
   end,
 })
 
@@ -71,7 +71,7 @@ cmp.setup({
         end
       }
     },
-    {name = 'nvim_lsp'},
+    {name = 'nvim_lsp', priority = 1000 },
     {
       name = 'tmux',
       option = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,6 +17,6 @@ telescope.setup({
 local nmap = vim.api.nvim_set_keymap
 nmap('n', '<leader>f',      ':lua require"telescope.builtin".treesitter({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',  {noremap = true, desc = 'Telescope Find with Treesitter' })
 nmap('n', '<leader>p',      ':lua require"telescope.builtin".find_files({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',  {noremap = true, desc = 'Telescope File Fuzzy Find' })
-nmap('n', '<leader>g',      ':Telescope live_grep({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',                        {noremap = true, desc = 'Telescope LiveGrep in CWD' })
+nmap('n', '<leader>g',      ':lua require"telescope.builtin".live_grep({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',   {noremap = true, desc = 'Telescope LiveGrep in CWD' })
 
 telescope.load_extension("fzf")
