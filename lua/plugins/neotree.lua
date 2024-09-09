ec898a0652720a99aa9fa1d603090f21dcce6f7d
Kevin Ran <kran@adobe.com>
2024-09-09 12:19:36 -0400

    chore: disable trash neovim fuzzy finder

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,3 +2,13 @@ local nmap = vim.api.nvim_set_keymap
 
 nmap('n', '<leader><tab>', ':Neotree toggle<CR>', {noremap = true, desc = 'Neotree toggle.' })
 nmap('n', '<leader><S-tab>', ':Neotree reveal<CR>', {noremap = true, desc = 'Neotree reveal file for buff.' })
+
+require("neo-tree").setup({
+  enable_diagnostics = false,
+  window = {
+    -- I hate this thing with a passion
+    mappings = {
+      ['/'] = 'noop'
+    }
+  }
+})
