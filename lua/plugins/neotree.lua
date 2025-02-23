b0c47702b481cc1ce97e62e7aa3afb67f8818ee2
chr0n1x <heilong24@gmail.com>
2025-02-22 22:37:40 -0500

    fix: broke my own neotree keybind

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,7 +1,7 @@
 local nmap = vim.api.nvim_set_keymap
 
 nmap('n', '<leader><S-tab>', ':Neotree toggle<CR>', {noremap = true, desc = 'Neotree toggle.' })
-nmap('n', '<leader><S>',     ':Neotree reveal<CR>', {noremap = true, desc = 'Neotree reveal file for buff.' })
+nmap('n', '<leader><tab>',   ':Neotree reveal<CR>', {noremap = true, desc = 'Neotree reveal file for buff.' })
 
 require("neo-tree").setup({
   enable_diagnostics = false,
