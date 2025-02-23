09b7d916da38b5d2072c19442694c6c4387671cd
chr0n1x <heilong24@gmail.com>
2025-02-22 22:31:45 -0500

    feat: trying out marks

--- lua/key-bindings.lua
+++ lua/key-bindings.lua
@@ -8,26 +8,26 @@ vim.g.mapleader = ' '
 -- nothing to let the which-key plugin list possible keymappings
 
 -- buffer navigation
-nmap('n', '<leader>s',       ':/<C-r><C-w>/<CR>',                               {noremap = true, desc = 'Search word underneath cursor' })
+nmap('n', '<leader>s', ':/<C-r><C-w>/<CR>', {noremap = true, desc = 'Search word underneath cursor.' })
 
 -- tab navigation
-nmap('n', '<leader>n',       ':tabnext<CR>',                                    {noremap = true, desc = 'Tab; next' })
-nmap('n', '<leader>b',       ':tabprevious<CR>',                                {noremap = true, desc = 'Tab; prev' })
-nmap('n', '<leader>o',       ':tabe<space>',                                    {noremap = true, desc = 'Tab; open' })
+nmap('n', '<leader>n', ':tabnext<CR>',     {noremap = true, desc = 'Tab; next' })
+nmap('n', '<leader>b', ':tabprevious<CR>', {noremap = true, desc = 'Tab; prev' })
+nmap('n', '<leader>o', ':tabe<space>',     {noremap = true, desc = 'Tab; open' })
 
 -- editor visuals & "ergonomics"
-nmap('n', '<leader>m',       ':set mouse=a<CR>',                                {noremap = true, desc = 'Turn ON mouse mode.' })
-nmap('n', '<leader>M',       ':set mouse=c<CR>',                                {noremap = true, desc = 'Turn OFF mouse mode.' })
+nmap('n', '<leader>M', ':set mouse!<CR>', {noremap = true, desc = 'Toggle mouse mode.' })
 
 -- misc 
-nmap('n', '<leader>w',       ':w<CR>',                                          {noremap = true, desc = 'Save buff. Or manage sessions.'})
-nmap('n', '<leader><space>', ':noh <bar> e<CR>',                                {noremap = true, desc = 'Close any highlights.'})
-nmap('n', '<leader>q',       ':q<CR>',                                          {noremap = true, desc = 'Close buffer (:q).'})
-nmap('n', 'er',              ':',                                               {noremap = true, desc = 'Quick-enter into command mode.'})
+nmap('n', '<leader>w',       ':w<CR>',                   {noremap = true, desc = 'Save buff. Or manage sessions.'})
+nmap('n', '<leader><space>', ':noh <bar> e<CR>',         {noremap = true, desc = 'Close any highlights.'})
+nmap('n', '<leader>q',       ':q<CR>',                   {noremap = true, desc = 'Close buffer (:q).'})
+nmap('n', '<leader>md',      ':delm! | delm A-Z0-9<CR>', {noremap = true, desc = 'Marks: Delete all.'})
+nmap('n', 'er',              ':',                        {noremap = true, desc = 'Quick-enter into command mode.'})
 
 -- utility scripts
 -- pretty-format JSON in the current buffer/file
-nmap('n', '<leader>J',       ':%!python3 -m json.tool --sort-keys<CR>',         {noremap = true, desc = 'Use python to pretty-format JSON' })
+nmap('n', '<leader>J', ':%!python3 -m json.tool --sort-keys<CR>', {noremap = true, desc = 'Use python to pretty-format JSON' })
 
 -- DEADGE
-nmap('n', '<leader>F',       ':CellularAutomaton make_it_rain<CR>',             {noremap = true, desc = 'Am sadge' })
+nmap('n', '<leader>F', ':CellularAutomaton make_it_rain<CR>', {noremap = true, desc = 'Am sadge' })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,5 +19,6 @@ nmap('n', '<leader>f', ':lua require"telescope.builtin".treesitter({ hidden = tr
 nmap('n', '<leader>g', ':lua require"telescope.builtin".live_grep({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',      {noremap = true, desc = 'Telescope: [rip]grep cwd.' })
 nmap('n', '<leader>p', ':lua require"telescope.builtin".find_files({ hidden = true, file_ignore_patterns = {".git/"} })<CR>',     {noremap = true, desc = 'Telescope: search cwd for file.' })
 nmap('n', '<leader>r', ':lua require"telescope.builtin".lsp_references({ hidden = true, file_ignore_patterns = {".git/"} })<CR>', {noremap = true, desc = 'Telescope: show references for token under cursor.' })
+nmap('n', '<leader>m', ':lua require"telescope.builtin".marks()<CR>',                                                             {noremap = true, desc = 'Telescope: show marks.' })
 
 telescope.load_extension("fzf")
