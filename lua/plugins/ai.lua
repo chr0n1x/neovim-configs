aca95ea00a48cf9f0f61ee10aa9d080ce5a1ca1b
chr0n1x <heilong24@gmail.com>
2025-04-06 15:51:32 -0400

    chore: bumping things

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -56,7 +56,7 @@ return {
             -- NOTE: startup time might be better with 1B
             -- 1080ti over proxmox pci passthrough to talos os is takes a few seconds for 4B
             -- and 12B takes FOREVER, t/s is also not too good
-            model = { default = "gemma3:1B" },
+            model = { default = "gemma3:4B" },
           },
         })
       end,
