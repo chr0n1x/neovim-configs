35db287c718f0c80fa63ead85859afc06238fcb6
Kevin Ran <kran@adobe.com>
2025-04-10 20:57:14 -0400

    feat: vectorise cmd in cwd

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -18,47 +18,41 @@ end
 
 local vectorcode_exists = pcall(run_shell_cmd, "which vectorcode")
 
+local deps = {
+  "nvim-lua/plenary.nvim",
+  "nvim-treesitter/nvim-treesitter",
+}
+if vectorcode_exists then
+  table.insert(deps, "Davidyz/VectorCode")
+end
+
+local cc_strats = {
+  chat = {
+    adapter = default_adapter,
+  },
+  inline = {
+    adapter = default_adapter,
+    keymaps = {
+      accept_change = {
+        modes = { n = "ga" },
+        description = "Accept the suggested change",
+      },
+      reject_change = {
+        modes = { n = "gr" },
+        description = "Reject the suggested change",
+      }
+    }
+  }
+}
+
 -- starting configuration here, above are just general flags
 
-return {
-  {
-    "Davidyz/VectorCode",
-    lazy = false,
-    build = "uvenv upgrade vectorcode",
-    version = "*",
-    dependencies = { "nvim-lua/plenary.nvim" },
-
-    config = function ()
-      if not vectorcode_exists then return end
-
-      local ok, cw_gitdir = pcall(run_shell_cmd, "git rev-parse --git-dir 2&> /dev/null")
-      if not ok or cw_gitdir == nil then
-        vim.notify(
-          "not in git repo, not initializing vectorcode; err: " .. cw_gitdir,
-          vim.log.levels.WARN
-        )
-        return
-      end
-
-      local vc_cmd = "vectorcode --project_root=" .. cw_gitdir:gsub("%s+", "") .. " init 2>&1 | tee"
-      local vectocode_started, vectorcode_init_out = pcall(run_shell_cmd, vc_cmd)
-      if vectocode_started then
-        vim.notify(vectorcode_init_out, vim.log.levels.INFO)
-      else
-        vim.notify(vectorcode_init_out, vim.log.levels.WARN)
-      end
-    end
-  },
-
+local ai_plugins = {
   {
     'olimorris/codecompanion.nvim',
     lazy = true,
     cmd = "CodeCompanionActions",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "Davidyz/VectorCode",
-    },
+    dependencies = deps,
 
     keys = {
       { '<leader>c', ':CodeCompanionActions<CR>', { desc = 'CodeCompanion: Actions.' } },
@@ -110,24 +104,6 @@ return {
         })
       end
 
-      local cc_strats = {
-        chat = {
-          adapter = default_adapter,
-        },
-        inline = {
-          adapter = default_adapter,
-          keymaps = {
-            accept_change = {
-              modes = { n = "ga" },
-              description = "Accept the suggested change",
-            },
-            reject_change = {
-              modes = { n = "gr" },
-              description = "Reject the suggested change",
-            }
-          }
-        }
-      }
       if vectorcode_exists then
         local vc_int = require("vectorcode.integrations")
         cc_strats.chat.slash_commands = { codebase = vc_int.codecompanion.chat.make_slash_command() }
@@ -153,3 +129,77 @@ return {
     end
   }
 }
+
+if vectorcode_exists then
+  local vectorise_codebase = function ()
+    local ext = vim.fn.expand('%:e')
+    local partial_glob = vim.fn.expand('%:h') .. "/**/*." .. ext
+    if #ext == 0 then
+      vim.notify("Not Vectorising, no files found in " .. partial_glob, vim.log.levels.ERROR)
+      return
+    end
+
+    local file_glob = vim.fn.expand('%:p:h') .. "/**/*." .. ext
+    vim.fn.jobstart(
+      'vectorcode vectorise ' .. file_glob,
+      {
+        on_exit = function()
+          vim.notify("vectorised code in " .. partial_glob)
+        end
+      }
+    )
+  end
+
+  table.insert(
+    ai_plugins,
+    {
+      "Davidyz/VectorCode",
+      lazy = false,
+      build = "uvenv upgrade vectorcode",
+      version = "*",
+      dependencies = { "nvim-lua/plenary.nvim" },
+
+      keys = {
+        { '<leader>v', ':VectorCode register<CR>', { desc = 'VectorCode' } },
+        { '<leader>vv', vectorise_codebase, { desc = 'vectorise current codebase.' } },
+      },
+
+      config = function ()
+        if not vectorcode_exists then return end
+
+        local ok, cw_gitdir = pcall(run_shell_cmd, "git rev-parse --git-dir 2&> /dev/null")
+        if not ok or cw_gitdir == nil then
+          vim.notify(
+            "not in git repo, not initializing vectorcode; err: " .. cw_gitdir,
+            vim.log.levels.WARN
+          )
+          return
+        end
+
+        local vc_cmd = "vectorcode --project_root=" .. cw_gitdir:gsub("%s+", "") .. " init 2>&1 | tee"
+        local vectocode_started, vectorcode_init_out = pcall(run_shell_cmd, vc_cmd)
+        if vectocode_started then
+          vim.notify(vectorcode_init_out, vim.log.levels.INFO)
+        else
+          vim.notify(vectorcode_init_out, vim.log.levels.WARN)
+        end
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+          callback = function()
+            local cacher = require("vectorcode").cacher
+            local bufnr = vim.api.nvim_get_current_buf()
+            cacher.async_check("config", function()
+              cacher.register_buffer(
+                bufnr,
+                { n_query = 10, }
+              )
+            end, nil)
+          end,
+          desc = "Register buffer for VectorCode",
+        })
+      end
+    }
+  )
+end
+
+return ai_plugins
