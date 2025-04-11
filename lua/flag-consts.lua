ecf27dc2ac3d2a5c3e2740f345c14c46ccf999e4
chr0n1x <heilong24@gmail.com>
2025-04-10 22:49:11 -0400

    fix: vectorcode cacher

---
+++ lua/flag-consts.lua
@@ -0,0 +1,22 @@
+-- prevent a bunch of plugins from loading when on a machine like...
+-- an rpi zero
+IN_PERF_MODE = os.getenv("NVIM_LAZY_N_LITE") == "true"
+DISABLED_IF_IN_PERF_MODE = not IN_PERF_MODE
+
+-- AI plugin load logic/flags
+-- plugins usually not loaded when IN_PERF_MODE == true
+-- defining here because multiple plugins depend on these
+
+-- open webui
+OPENWEBUI_URL = os.getenv("OPEN_WEBUI_URL")
+OPENWEBUI_JWT = os.getenv("OPEN_WEBUI_JWT")
+OPENWEBUI_DISABLED = (
+  OPENWEBUI_JWT == "" or OPENWEBUI_JWT == nil or
+  OPENWEBUI_URL == "" or OPENWEBUI_URL == nil
+)
+OPENWEBUI_ENABLED = not OPENWEBUI_DISABLED
+
+-- ollama
+OLLAMA_URL = os.getenv("OLLAMA_HOST")
+OLLAMA_DISABLED = OLLAMA_URL == "" or OLLAMA_URL == nil
+OLLAMA_ENABLED = not OLLAMA_DISABLED
--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -15,7 +15,6 @@ local default_adapter = "ollama"
 if OPENWEBUI_ENABLED then
   default_adapter = "gemma3"
 end
-
 local vectorcode_exists = pcall(run_shell_cmd, "which vectorcode")
 
 local deps = {
@@ -119,12 +118,17 @@ local ai_plugins = {
       require('codecompanion').setup(opts)
 
       if OPENWEBUI_ENABLED then
-        vim.notify('codecompanion adapter ' .. default_adapter .. 'AI via ' .. OPENWEBUI_URL .. ' enabled', vim.log.levels.INFO)
+        vim.notify(
+          'codecompanion adapter ' ..
+            default_adapter .. ' AI via ' ..
+            OPENWEBUI_URL .. ' enabled',
+          vim.log.levels.INFO
+        )
         return
       end
 
       if OLLAMA_ENABLED then
-        vim.notify('codecompanion adapter ' .. default_adapter .. 'AI via ' .. OLLAMA_URL .. ' enabled', vim.log.levels.INFO)
+        vim.notify('codecompanion adapter ' .. default_adapter .. ' AI via ' .. OLLAMA_URL .. ' enabled', vim.log.levels.INFO)
       end
     end
   }
@@ -184,19 +188,22 @@ if vectorcode_exists then
           vim.notify(vectorcode_init_out, vim.log.levels.WARN)
         end
 
-        -- vim.api.nvim_create_autocmd("LspAttach", {
-        --   callback = function()
-        --     local cacher = require("vectorcode").cacher
-        --     local bufnr = vim.api.nvim_get_current_buf()
-        --     cacher.async_check("config", function()
-        --       cacher.register_buffer(
-        --         bufnr,
-        --         { n_query = 10, }
-        --       )
-        --     end, nil)
-        --   end,
-        --   desc = "Register buffer for VectorCode",
-        -- })
+        vim.api.nvim_create_autocmd(
+          'LspAttach',
+          {
+            callback = function()
+              local cacher = require("vectorcode.config").get_cacher_backend()
+              local bufnr = vim.api.nvim_get_current_buf()
+              cacher.async_check("config", function()
+                cacher.register_buffer(
+                  bufnr,
+                  { n_query = 10, }
+                )
+              end, nil)
+            end,
+            desc = "Register buffer for VectorCode",
+          }
+        )
       end
     }
   )
