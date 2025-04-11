c9a9ee0530f04cf3aebdc3203f24f7bab1a84105
Kevin Ran <kran@adobe.com>
2025-04-11 12:45:27 -0400

    fix: more flag fixes

--- lua/flag-consts.lua
+++ lua/flag-consts.lua
@@ -23,11 +23,11 @@ OLLAMA_URL = os.getenv("OLLAMA_HOST")
 OLLAMA_DISABLED = OLLAMA_URL == "" or OLLAMA_URL == nil
 OLLAMA_ENABLED = not OLLAMA_DISABLED
 OLLAMA_DEFAULT_MODEL = 'qwen2.5-coder:7b-base-q6_K'
-local _, ollama_qwen_pulled_ec = pcall(
+local ok, out = pcall(
   RUN_SHELL_CMD, 'ollama ls | grep ' .. OLLAMA_DEFAULT_MODEL
 )
-OLLAMA_MODEL_PRESENT = (ollama_qwen_pulled_ec == 0)
-OLLAMA_MODEL_NOT_PRESENT = not OLLAMA_MODEL_PRESENT
+OLLAMA_MODEL_NOT_PRESENT = not ok or out == "" or out == nil
+OLLAMA_MODEL_PRESENT = not OLLAMA_MODEL_NOT_PRESENT
 
 -- where we actually decide _WHICH_ LLM provider/engine to use
 -- I don't want to enable two at a time
