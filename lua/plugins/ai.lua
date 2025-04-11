3b2693a876b76a669b8ef21043dbf7145059cea6
Kevin Ran <kran@adobe.com>
2025-04-10 20:58:15 -0400

    fix: oops, pushed up lspattach

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -184,19 +184,19 @@ if vectorcode_exists then
           vim.notify(vectorcode_init_out, vim.log.levels.WARN)
         end
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-          callback = function()
-            local cacher = require("vectorcode").cacher
-            local bufnr = vim.api.nvim_get_current_buf()
-            cacher.async_check("config", function()
-              cacher.register_buffer(
-                bufnr,
-                { n_query = 10, }
-              )
-            end, nil)
-          end,
-          desc = "Register buffer for VectorCode",
-        })
+        -- vim.api.nvim_create_autocmd("LspAttach", {
+        --   callback = function()
+        --     local cacher = require("vectorcode").cacher
+        --     local bufnr = vim.api.nvim_get_current_buf()
+        --     cacher.async_check("config", function()
+        --       cacher.register_buffer(
+        --         bufnr,
+        --         { n_query = 10, }
+        --       )
+        --     end, nil)
+        --   end,
+        --   desc = "Register buffer for VectorCode",
+        -- })
       end
     }
   )
