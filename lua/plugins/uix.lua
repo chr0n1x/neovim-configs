bdb4b87aef406ac98a7abe0a965d83355e83daae
Kevin Ran <kran@adobe.com>
2025-04-11 20:44:08 -0400

    chore: lsp prog on lualine + clr notifications

--- lua/plugins/uix.lua
+++ lua/plugins/uix.lua
@@ -9,7 +9,7 @@ local uix_plugins = {
     keys = {
       { '<leader>e', ':Telescope notify<CR>', { desc = "View notifications in Telescope." } },
       { '<leader>ee', ':Telescope notify<CR>', { desc = "View notifications in Telescope." } },
-      { '<leader>ec', ':NotificationsClear<CR>', { desc = "Clear notifications." } },
+      { '<leader>ec', ':lua require("notify").dismiss()<CR>', { desc = "Clear notifications." } },
     }
   },
 
@@ -44,7 +44,25 @@ local uix_plugins = {
         lualine_a = {'mode'},
         lualine_b = {'branch'},
         lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          'filetype',
+          {
+            'lsp_status',
+            icon = '', -- f013
+            symbols = {
+              -- Standard unicode symbols to cycle through for LSP progress:
+              spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+              -- Standard unicode symbol for when LSP is done:
+              done = '✓',
+              -- Delimiter inserted between LSP names:
+              separator = ' ',
+            },
+            -- List of LSP names to ignore (e.g., `null-ls`):
+            ignore_lsp = {},
+          }
+        },
         lualine_y = {'progress'},
         lualine_z = {'location'}
       },
