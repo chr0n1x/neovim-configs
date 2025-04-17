8840004a76e8ba489f1442544ebb8e4d8fa61fd2
chr0n1x <heilong24@gmail.com>
2025-04-17 03:25:22 -0400

    chore: split out AI plugins

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,132 @@
+if IN_PERF_MODE then return {} end
+if OLLAMA_DISABLED and OPENWEBUI_DISABLED then return {} end
+
+-- notification things
+local setup_notification_cfg = {
+  title = "AI Plugin Setup",
+  style = "minimal",
+  timeout = 1000,
+}
+
+local deps = {
+  "nvim-lua/plenary.nvim",
+  "nvim-treesitter/nvim-treesitter",
+}
+if VECTORCODE_INSTALLED then
+  table.insert(deps, "Davidyz/VectorCode")
+end
+
+local cc_strats = {
+  chat = {
+    adapter = DEFAULT_AI_ADAPTER,
+  },
+  inline = {
+    adapter = DEFAULT_AI_ADAPTER,
+    keymaps = {
+      accept_change = {
+        modes = { n = "ga" },
+        description = "Accept the suggested change",
+      },
+      reject_change = {
+        modes = { n = "gr" },
+        description = "Reject the suggested change",
+      }
+    }
+  }
+}
+
+if OLLAMA_ENABLED then
+  cc_strats.inline.adapter = OLLAMA_ADAPTER_NAME
+end
+if OPENWEBUI_ENABLED then
+  cc_strats.chat.adapter = OPENWEBUI_ADAPTER_NAME
+end
+
+return {
+  'olimorris/codecompanion.nvim',
+  lazy = true,
+  cmd = "CodeCompanionActions",
+  dependencies = deps,
+
+  keys = {
+    { '<leader>c', ':CodeCompanionActions<CR>', desc = 'CodeCompanion: Actions.' },
+  },
+
+  config = function (_, opts)
+    opts = opts or {}
+
+    opts.adapters = opts.adapters or {}
+
+    if OPENWEBUI_ENABLED then
+      opts.adapters["openwebui"] = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          opts = {
+            show_defaults = true,
+            display = { show_settings = true }
+          },
+
+          env = {
+            url = OPENWEBUI_URL,
+            api_key = OPENWEBUI_JWT,
+            chat_url = "/api/chat/completions",
+            models_endpoint = "/api/models",
+          },
+          schema = {
+            model = { default = OPENWEBUI_MODEL },
+          },
+        })
+      end
+    end
+
+    if OLLAMA_ENABLED then
+      opts.adapters["ollama"] = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          model = OLLAMA_MODEL,
+          opts = {
+            allow_insecure = true,
+            show_defaults = true,
+          },
+          env = {
+            url = OLLAMA_URL,
+            api_key = OLLAMA_API_KEY,
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+            ["Authorization"] = "Bearer ${api_key}",
+          },
+          parameters = { sync = true },
+        })
+      end
+    end
+
+    -- will not work if moved outside of config setup fx
+    if VECTORCODE_INSTALLED then
+      local vc_int = require("vectorcode.integrations")
+      cc_strats.chat.slash_commands = { codebase = vc_int.codecompanion.chat.make_slash_command() }
+      cc_strats.chat.tools = {
+        vectorcode = {
+          description = "Run VectorCode to retrieve the project context.",
+          callback = vc_int.codecompanion.chat.make_tool(),
+        }
+      }
+    end
+
+    opts.strategies = cc_strats
+    require('codecompanion').setup(opts)
+
+    local statusmsg = 'codecompanion AI adapter(s) configured:\n\n'
+    if OPENWEBUI_ENABLED then
+      statusmsg = statusmsg .. '> ' .. OPENWEBUI_MODEL .. ' via ' ..
+      OPENWEBUI_URL .. ' (' .. OPENWEBUI_ADAPTER_NAME .. ') \n'
+    end
+    if OLLAMA_ENABLED then
+      statusmsg = statusmsg .. '> ' .. OLLAMA_MODEL .. ' via ' ..
+      OLLAMA_URL .. ' (' .. OLLAMA_ADAPTER_NAME .. ') \n'
+    end
+    statusmsg = statusmsg .. "\n(cmp-ai is separate)"
+
+    if OLLAMA_ENABLED or OPENWEBUI_ENABLED then
+      vim.notify(statusmsg, vim.log.levels.INFO, setup_notification_cfg)
+    end
+  end
+}
---
+++ lua/plugins/vectorcode.lua
@@ -0,0 +1,113 @@
+if IN_PERF_MODE then return {} end
+if OLLAMA_DISABLED and OPENWEBUI_DISABLED then return {} end
+if VECTORCODE_NOT_INSTALLED then return {} end
+
+local task_notifications = require('../util/task_notifications')
+
+local vc_notification_cfg = { title = "VectorCode", render = "compact" }
+
+local vectorise_codebase = function()
+  local ext = vim.fn.expand('%:e')
+  local partial_glob = vim.fn.expand('%:h') .. "/**/*." .. ext
+  if #ext == 0 then
+    vim.notify(
+      "Not Vectorising, no files found in " .. partial_glob,
+      vim.log.levels.ERROR,
+      vc_notification_cfg
+    )
+    return
+  end
+
+  local file_glob = vim.fn.expand('%:p:h') .. "/**/*." .. ext
+  local task_name = "VectorCoderizing Codebase"
+  local msg = "running `vectorcode vectorise` in " .. partial_glob
+  local stderr = {}
+  local stdout = {}
+
+  task_notifications.start(task_name, msg)
+  vim.fn.jobstart(
+    'vectorcode vectorise ' .. file_glob,
+    {
+      on_stdout = function(chanid, data, name)
+        table.insert(stdout, { chanid = chanid, data = data, name = name })
+      end,
+      on_stderr = function(chanid, data, name)
+        table.insert(stderr, { chanid = chanid, data = data, name = name })
+      end,
+      on_exit = function()
+        task_notifications.clear(task_name)
+
+        local serialize_stdtxt = function(tbl)
+          local out = ""
+          for _, entry in ipairs(tbl) do
+            for _, txt in ipairs(entry.data) do
+              out = out .. '\n' .. txt
+            end
+          end
+          return out
+        end
+
+        vim.notify(
+          "stdout: " .. serialize_stdtxt(stdout),
+          vim.log.levels.DEBUG,
+          { title = "VectorCode 'vectorise' output" }
+        )
+        vim.notify(
+          "stderr: " .. serialize_stdtxt(stderr),
+          vim.log.levels.WARN,
+          { title = "VectorCode 'vectorise' StdErr" }
+        )
+      end
+    }
+  )
+end
+
+return {
+  "Davidyz/VectorCode",
+  lazy = false,
+  build = "uvenv upgrade vectorcode",
+  -- version = "*",
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  keys = {
+    { '<leader>v', ':VectorCode register<CR>', desc = 'VectorCode register' },
+    { '<leader>vv', vectorise_codebase, desc = 'vectorise current codebase.' },
+  },
+  opts = function()
+    return {
+      async_backend = "lsp",
+      notify = true,
+      on_setup = { lsp = false },
+      n_query = 10,
+      timeout_ms = -1,
+      async_opts = {
+        events = { "BufWritePost" },
+        single_job = true,
+        query_cb = require("vectorcode.utils").make_surrounding_lines_cb(40),
+        debounce = -1,
+        n_query = 30,
+      },
+    }
+  end,
+
+  config = function ()
+    vim.api.nvim_create_autocmd(
+      'LspAttach',
+      {
+        callback = function()
+          local cacher = require("vectorcode.config").get_cacher_backend()
+          local bufnr = vim.api.nvim_get_current_buf()
+          cacher.async_check("config", function()
+            cacher.register_buffer(bufnr, { n_query = 10, })
+            -- local query_results = cacher.query_from_cache(0, {notify=true})
+            -- vim.notify(query_results)
+          end, nil)
+        end,
+        desc = "Register buffer for VectorCode",
+      }
+    )
+  end,
+
+  cmd = "VectorCode",
+  cond = VECTORCODE_INSTALLED
+}
