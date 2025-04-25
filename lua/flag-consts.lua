e423e9b02cf7af09be6306c60e4e843f9365f60b
chr0n1x <heilong24@gmail.com>
2025-04-24 22:44:35 -0400

    chore: prevent err message

--- lua/flag-consts.lua
+++ lua/flag-consts.lua
@@ -29,9 +29,7 @@ OLLAMA_API_KEY = os.getenv("OLLAMA_API_KEY") or ""
 OLLAMA_DISABLED = OLLAMA_URL == "" or OLLAMA_URL == nil
 OLLAMA_ENABLED = not OLLAMA_DISABLED
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL") or 'qwen2.5-coder:7b-base-q6_K'
-local ok, out = pcall(
-  RUN_SHELL_CMD, 'ollama ls | grep ' .. OLLAMA_MODEL
-)
+local ok, out = RUN_SHELL_CMD('ollama ls | grep ' .. OLLAMA_MODEL)
 OLLAMA_MODEL_NOT_PRESENT = not ok or out == "" or out == nil
 OLLAMA_MODEL_PRESENT = not OLLAMA_MODEL_NOT_PRESENT
 OLLAMA_ADAPTER_NAME = "ollama"
