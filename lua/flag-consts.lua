ea5e9ffcacdade3a72821d9526b406ed19b6593f
chr0n1x <heilong24@gmail.com>
2025-07-01 16:48:05 -0400

    chore: change default coder model; save vram

--- lua/flag-consts.lua
+++ lua/flag-consts.lua
@@ -27,7 +27,7 @@ OLLAMA_API_KEY = os.getenv("OLLAMA_API_KEY") or ""
 OLLAMA_NVIM_DISABLED = os.getenv("OLLAMA_NVIM_DISABLED") or ""
 OLLAMA_DISABLED = OLLAMA_URL == "" or OLLAMA_URL == nil or OLLAMA_NVIM_DISABLED == "true"
 OLLAMA_ENABLED = not OLLAMA_DISABLED
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL") or 'qwen2.5-coder:7b-base-q6_K'
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL") or 'qwen2.5-coder:7b-base-q6_K' -- 7b-base is Q4
 local exit = os.execute('ollama ls | grep ' .. OLLAMA_MODEL) / 256
 OLLAMA_MODEL_NOT_PRESENT = (exit == 1)
 OLLAMA_MODEL_PRESENT = not OLLAMA_MODEL_NOT_PRESENT
