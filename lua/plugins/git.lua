579f3e393e0f428efddd7ad35e027ea1f21704ff
chr0n1x <heilong24@gmail.com>
2025-08-04 20:47:19 -0400

    feat: neogit and signs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,6 +14,55 @@ return {
   },
 
   {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    config = function()
+      require("neogit").setup({
+        kind = "split",
+        signs = {
+          section = { "", "" },
+          item = { "", "" },
+          hunk = { "", "" },
+        },
+        integrations = { diffview = true },
+      })
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+          change = {
+            hl = "GitSignsChange",
+            text = "│",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+          },
+          delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+          topdelete = {
+            hl = "GitSignsDelete",
+            text = "‾",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+          },
+          changedelete = {
+            hl = "GitSignsChange",
+            text = "~",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+          },
+          untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        },
+      })
+    end,
+  },
+
+  {
     'mrloop/telescope-git-branch.nvim',
     config = function ()
       vim.keymap.set('n', '<leader>df', require('git_branch').files , { desc = 'Telescope: Show git diff.' })
