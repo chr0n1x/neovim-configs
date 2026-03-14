2b8c8f85af98b502c04f9d070d0fdf27ba322972
Kevin R <heilong24@gmail.com>
2026-03-13 20:57:05 -0400

    chore: imma hate myself tomorrow

--- lua/plugins/uix.lua
+++ lua/plugins/uix.lua
@@ -141,7 +141,31 @@ if not IN_PERF_MODE then
   table.insert(
     uix_plugins,
     -- initially did not like this, but very useful for REALLY big screens
-    { "sphamba/smear-cursor.nvim", opts = {} }
+    {
+      "sphamba/smear-cursor.nvim",
+      -- FIREEEEEEE
+      opts = {
+        cursor_color = "#5DADE2",
+        particles_enabled = true,
+        stiffness = 0.5,
+        trailing_stiffness = 0.2,
+        trailing_exponent = 5,
+        damping = 0.6,
+        gradient_exponent = 0,
+        gamma = 1,
+        never_draw_over_target = true, -- if you want to actually see under the cursor
+        hide_target_hack = true,       -- same
+        particle_spread = 1,
+        particles_per_second = 500,
+        particles_per_length = 50,
+        particle_max_lifetime = 800,
+        particle_max_initial_velocity = 20,
+        particle_velocity_from_cursor = 0.5,
+        particle_damping = 0.15,
+        particle_gravity = -50,
+        min_distance_emit_particles = 0,
+      }
+    }
   )
 
   table.insert(
