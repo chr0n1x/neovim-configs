9f424ce267cf59131b8555623c65cb19f871852c
Kevin R <heilong24@gmail.com>
2026-03-18 21:34:47 -0400

    chore: force close floating panes cause I cant

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -5,16 +5,34 @@ return {
     'nvim-lua/plenary.nvim'
   },
   opts = {
-    autoload = "auto_cwd",
+    autoload = 'auto_cwd',
     autosave = {
       cwd = true,
       current = true,
       on_load = true,
       on_quit = true,
     },
+    plugins = {
+      close_windows = {
+        hooks = { 'before_save', 'before_load' },
+        preserve_layout = true,
+        match = {
+          floating = false,
+          buftype = {},
+          filetype = {},
+        },
+      },
+      delete_hidden_buffers = {
+        hooks = {
+          'before_load',
+          vim.o.sessionoptions:match('buffer') and 'before_save',
+        },
+        force = true,
+      },
+    },
   },
   keys = {
-    { "<leader>sl", ":PossessionListCwd<CR>", desc = "📌 Show cwd session." },
-    { "<leader>s<CR>", ":PossessionSave<CR>", desc = "📌 Save current session" },
+    { '<leader>sl', '<cmd>PossessionListCwd<cr>', desc = '📌 Show cwd session.' },
+    { '<leader>s', '<cmd>PossessionSave<cr>', desc = '📌 Save current session' },
   },
 }
