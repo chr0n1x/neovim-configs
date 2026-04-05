34434975d82b24340bc5c6b5a7beb058e1c0c018
Kevin R. <heilong24@gmail.com>
2026-04-05 18:55:57 -0400

    chore: more cleaning on fresh 0.12 install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,60 @@
 if IN_PERF_MODE then return {} end
 
 return {
-  'nvim-treesitter/nvim-treesitter',
-  requires = {
-    { 'windwp/nvim-ts-autotag' },
-    { 'nvim-treesitter/playground' }
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    -- not sure why this doesnt work ootb
+    -- have to go into ~/.local/share/nvim/lazy/telescope-fzf-native.nvim/ && make
+    build = "make",
   },
-  build = ':TSUpdate',
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        'vim', 'lua', 'bash', 'yaml',
-        'json', 'hcl', 'make', 'go',
-        'typescript', 'markdown', 'markdown_inline',
-        'bash', 'ruby', 'python', 'helm'
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
-  opts = {
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-      use_languagetree = true
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      { 'windwp/nvim-ts-autotag' },
+      { 'nvim-treesitter/playground' },
+      { "nvim-telescope/telescope-fzf-native.nvim" }
     },
-    indent = {
-      enable = true
-    },
-    autotag = { enable = true },
-    endwise = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+    build = ':TSUpdate',
+
+    -- TODO: not sure what to do yet
+    --
+    -- config = function ()
+    --   local configs = require("nvim-treesitter.configs")
+    --   configs.setup({
+    --     ensure_installed = {
+    --       'vim', 'lua', 'bash', 'yaml',
+    --       'json', 'hcl', 'make', 'go',
+    --       'typescript', 'markdown', 'markdown_inline',
+    --       'bash', 'ruby', 'python', 'helm'
+    --     },
+    --     sync_install = false,
+    --     highlight = { enable = true },
+    --     indent = { enable = true },
+    --     extensions_list = {'fzf'},
+    --   })
+    -- end,
+
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+        use_languagetree = true
       },
+      indent = {
+        enable = true
+      },
+      autotag = { enable = true },
+      endwise = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      }
     }
   }
 }
